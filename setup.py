334070eaafc50f6b0a873e59c2f0d6e85b1580c0
Ryan Finnie <ryan@finnie.org>
2020-06-22 11:38:43 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+
+setup(
+    name="rf-pymods",
+    description="A collection of small Python modules",
+    author="Ryan Finnie",
+    author_email="ryan@finnie.org",
+    packages=["rf_pymods"],
+)
