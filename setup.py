2f23e6122ac41cfbccbc07b8759d61760a200758
Ryan Finnie <ryan@finnie.org>
2026-05-01 10:49:26 -0700

    Bump requires-python; add explicit build-system; add stub setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+# SPDX-PackageName: rf-pymods
+# SPDX-PackageSupplier: Ryan Finnie <ryan@finnie.org>
+# SPDX-PackageDownloadLocation: https://codeberg.org/rfinnie/rf-pymods
+# SPDX-FileCopyrightText: © 2026 Ryan Finnie <ryan@finnie.org>
+# SPDX-License-Identifier: MIT
+
+from setuptools import setup
+
+setup()
